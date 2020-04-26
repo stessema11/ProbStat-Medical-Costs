cd03206044804af2875fd9fd1b23b73011e1ecf1
unknown <samsont@morantug.com>
2020-04-25 23:09:08 -0400

    initial plots

--- medical_costs.py
+++ medical_costs.py
@@ -2,13 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.metrics import mean_squared_error
 
-data = pd.read_csv("insurance.csv")
+data = pd.read_csv('insurance.csv')
 
 age = data.groupby(by = 'age').size()
 sex = data.groupby(by = 'sex').size()
@@ -18,42 +17,107 @@ smoker = data.groupby(by = 'smoker').size()
 region = data.groupby(by = 'region').size()
 charges = data.groupby(by = 'charges').size()
 
-distPlot = sns.distplot(data['charges'])
-plt.title("Distirbution of Charges")
-plt.show(distPlot)
+floatDataPoints = ['age', 'bmi', 'children', 'charges']
+stringDataPoints = ['sex', 'smoker', 'region']
+
+def get_distribution_plot(data, x):
+	'''
+	Generate distrbution of each data point
+	'''
+	distPlot = sns.distplot(data[x])
+	title_x = get_title(x)
+	plt.title('Distirbution of ' + title_x)
+	plt.show()
 
 
-def individual_plots(data):
+def get_bar_chart(data, x):
+	'''
+	Generate count of each data point
+	'''
+	plt.hist(data[x])
+	plt.title('Frequency of Person by ' + x)
+	plt.show()
+
+
+def make_plots(data, z = None):
 	'''
 	Generate plots of charges versus each data point on its own
+	Inputs:
+	data: from insurance.csv
+	z: add a color scale
+	Will generate either a scatter plot or violin plot depending on the data type
+		Float/Integer: Scatter Plot
+		String/Boolean: Violin Plot
 	'''
-	g = sns.jointplot(x = "age", y = "charges", data=data)
-	jointPlot1 = g.plot_joint(plt.scatter)
-	plt.show(jointPlot1)
+	for point in floatDataPoints:
+		if point != 'charges':
+			make_scatter_plot(data, point, z = z)
 
-	meanGender = data.groupby(by = "sex")["charges"].mean()
-	print(meanGender)
-	print(meanGender["male"] - meanGender["female"])
-	boxPlot1 = sns.violinplot(x = "sex", y = "charges", data = data)
-	plt.show(boxPlot1)
+	for point in stringDataPoints:
+		if z == 'smoker' or z == None:
+			make_violin_plot(data, point, z = z)
 
-	g2 = sns.jointplot(x = "bmi", y = "charges", data=data)
-	jointPlot2 = g2.plot_joint(plt.scatter)
-	plt.show(jointPlot2)
 
-	g3 = sns.jointplot(x = "children", y="charges", data=data)
-	jointPlot3 = g3.plot_joint(plt.scatter)
-	plt.show(jointPlot3)
+def multi_plots(data):
+	'''
+	Generate all individual plots with a colored z-axis for smokers, bmi and age
+	'''
+	make_plots(data, z = 'smoker')
+	make_plots(data, z = 'bmi')
+	make_plots(data, z = 'age')
 
-	meanSmoker = data.groupby(by = "smoker")["charges"].mean()
-	print(meanSmoker)
-	print(meanSmoker["yes"] - meanSmoker["no"])
-	boxPlot2 = sns.violinplot(x = "smoker", y = "charges", data = data)
-	plt.show(boxPlot2)
 
-	meanRegion = data.groupby(by = "region")["charges"].mean()
-	print(meanRegion)
-	boxPlot3 = sns.violinplot(x = "region", y = "charges", data = data)
-	plt.show(boxPlot3)
+def make_scatter_plot(data, x, y = 'charges', z = None):
+	if z is not None:
+		c = []
+		if z == 'smoker':
+			for i in data[z]:
+				if i == 'yes':
+					c.append('red')
+				else:
+					c.append('blue')
+		else:
+			c = data[z]
+	else:
+		c = None
 
-individual_plots(data)
+	plt.scatter(data[x], data[y], c = c)
+	if z is not None and z != 'smoker':
+		cbar = plt.colorbar()
+		cbar.ax.set_ylabel(z)
+	title_x = get_title(x)
+	plt.title(y.capitalize() + ' vs. ' + title_x)
+	plt.xlabel(x)
+	plt.ylabel(y)
+	plt.show()
+
+
+def make_violin_plot(data, x, y = 'charges', z = None):
+	mean = data.groupby(by = x)[y].mean()
+	hue = 'smoker' if z else None
+	violinPlot = sns.violinplot(x = x, y = y, data = data, hue = hue)
+	plt.show(violinPlot)
+
+
+def get_title(x):
+	if x == 'bmi':
+		title_x = x.upper()
+	elif x == 'children':
+		title_x = '# of ' + x.capitalize()
+	else:
+		title_x = x.capitalize()
+	return title_x
+
+
+def main():
+	for point in floatDataPoints:
+		get_distribution_plot(data, point)
+	for point in stringDataPoints:
+		get_bar_chart(data, point)
+
+	make_plots(data)
+	multi_plots(data)
+
+
+if __name__ == '__main__':
+	main()
